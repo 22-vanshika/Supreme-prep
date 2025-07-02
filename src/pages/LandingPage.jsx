3394cb94b79e481b8689857c568c615cdc93766b
vanshika sharma <vanshikas2022@gmail.com>
2025-07-02 11:09:12 +0530

    Add initial settings

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import hero_img from "../assets/hero-img.png"
+import {APP_FEATURES} from "../utils/data"
+const LandingPage = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export default LandingPage
---
+++ src/utils/data.js
@@ -0,0 +1,32 @@
+export const APP_FEATURES = [
+  {
+    id: "01",
+    title: "Tailored Just for You",
+    description:
+      "Receive personalized interview questions and model answers based on your role, experience, and focus areas.",
+  },
+  {
+    id: "02",
+    title: "Learn at Your Own Pace",
+    description:
+      "Expand answers only when you're ready. Explore concepts in depth with AI-powered, on-demand explanations.",
+  },
+  {
+    id: "03",
+    title: "Capture Your Insights",
+    description:
+      "Add personal notes to any question and pin important ones to the top—keeping your learning organized and focused.",
+  },
+  {
+    id: "04",
+    title: "Understand the 'Why' Behind Answers",
+    description:
+      "Go beyond surface-level responses. Unlock detailed, AI-generated explanations to truly master every concept.",
+  },
+  {
+    id: "05",
+    title: "Save, Organize & Revisit",
+    description:
+      "Bookmark your key questions, organize them by topic, and revisit anytime to reinforce your understanding.",
+  },
+];
