c78062d8265638a3f21cb67a61372e82342c4bd2
vanshika sharma <vanshikas2022@gmail.com>
2025-07-10 23:33:04 +0530

    Add Further implementations

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+const Login = ({ setCurrentPage }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+        Please enter your details to log in
+      </p>
+      <form onSubmit={handleLogin}></form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,6 +3,9 @@ import hero_img from "../assets/hero-img.png";
 import { APP_FEATURES } from "../utils/data";
 import { useNavigate } from "react-router-dom";
 import { LuSparkles } from "react-icons/lu";
+import Modal from "../components/Modal";
+import Login from "./Auth/Login";
+import SignUp from "./Auth/SignUp";
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -69,15 +72,80 @@ const LandingPage = () => {
         </div>
       </div>
 
-      <div className="w-full min-h-full relative z-10 mb-56">
+      <div className="w-full min-h-full relative z-10">
         <div>
           <section className="flex items-center justify-center -mt-36">
-            <img src={hero_img} alt="Hero Image" className="w-[80vw] rounded-2xl" />
+            <img
+              src={hero_img}
+              alt="Hero Image"
+              className="w-[80vw] rounded-2xl"
+            />
           </section>
+        </div>
+
+        <div className="w-full min-h-full bg-[#FFFCEF] mt-10">
+          <div className="container mx-auto px-4 pt-10 pb-20">
+            <section className="mt-5">
+              <h2 className="text-3xl font-medium text-center mb-12">
+                Features That Make You Shine
+              </h2>
+
+              <div className="flex flex-col items-center gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full">
+                  {APP_FEATURES.slice(0, 3).map((feature) => (
+                    <div
+                      key={feature.id}
+                      className="bg-[#FFFEF8] p-6 rounded-xl shadow-xs hover:shaadow-lg shadow-amber-100 transition border border-amber-100"
+                    >
+                      <h3 className="text-base font-semibold mb-3">
+                        {feature.title}
+                      </h3>
+                      <p className="text-gray-600">{feature.description}</p>
+                    </div>
+                  ))}
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                  {APP_FEATURES.slice(3).map((feature) => (
+                    <div
+                      key={feature.id}
+                      className="bg-[#FFFEF8] p-6 rounded-xl shadow-xs hover:shadow-lg shadow-amber-100 transition border border-amber-100"
+                    >
+                      <h3 className="text-base font-semibold mb-3">
+                        {feature.title}
+                      </h3>
+                      <p className="text-gray-600">{feature.description}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+
+        <div className="text-sm bg-gray-50 text-secondary text-center p-5 mt-5">
+          Made with 🧡 .. Happy Coding
         </div>
       </div>
 
-      
+      <Modal
+        isOpen={openAuthModal}
+        onClose={()=> {
+          setOpenAuthModal(false);
+          setCurrentPage("login");
+        }}
+        hideHeader>
+
+          <div>
+            {currentPage === "login" &&  (
+              <Login setCurrentPage={setCurrentPage}/>
+            )}
+            {currentPage === "login" &&  (
+              <SignUp setCurrentPage={setCurrentPage}/>
+            )}
+            
+          </div>
+      </Modal>
     </>
   );
 };
